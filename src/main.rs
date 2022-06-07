b115e4f16bec4ea14775c95f8ba0cc894d01751b
Avraham Appel <aappel248@gmail.com>
2022-06-07 17:44:56 -0400

    Better background color

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use tetra::{ContextBuilder, State};
+use tetra::graphics::{self, Color};
+use tetra::{ContextBuilder, Result as TetraResult, State};
 
 struct GameState;
 
-impl State for GameState {}
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut tetra::Context) -> TetraResult {
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+        Ok(())
+    }
+}
 
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", 640, 480)
